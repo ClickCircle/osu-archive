78663193c2859ec4a7305d90dbdac1fc71f493db
zhangjunmao <zhangjunmao@yingzi.com>
2019-03-06 16:41:42 +0800

    Init

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 data = {}
 
+$(document).ready(function(){
+    var url = "https://rrtyui.github.io/osu-archive/osu_standard.csv";
+    var htmlobj= $.ajax({url:url,async:false});
+});
+
 function draw(data) {
 	
 }
