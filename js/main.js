78e6d5a66414f3e4bb10ca3ff78eab44e485fa99
Kaya <1010955096@qq.com>
2019-03-06 16:52:55 +0800

    Init

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 data = {}
 
 $(document).ready(function(){
-    var url = "https://rrtyui.github.io/osu-archive/osu_standard.csv";
+    var url = "https://rrtyui.github.io/osu-archive/data/osu_standard.csv";
     var htmlobj= $.ajax({url:url,async:false});
 });
 
